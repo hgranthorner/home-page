f6dbb01d25002e0e24533cf681c99aae34d05843
Grant <h.grant.horner@gmail.com>
2021-07-05 15:51:45 -0400

    add basic outline

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('views', './src')
+
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.render('index')
 })
 
 app.listen(port, () => {
